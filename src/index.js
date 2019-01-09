02a024e4ad601201859ccf80d715cecd497d26bb
Chris Metzger <chrismetzgr@gmail.com>
2019-01-09 09:32:20 -0500

    Search bar feature

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
-  //YOUR CODE HERE
+
+  const searchBar = document.getElementById("pokemon-search-input")
+  const pokemonContainer = document.getElementById("pokemon-container")
+
+  function makePokemonCard(pokemon) {
+    return `<div className="pokemon-card">
+              <p>${pokemon.name}</p>
+              <img id=${pokemon.name + "-sprite"} src=${pokemon.sprites.front}>
+             </div>`
+  }
+
+  function loadPokemonToDOM() {
+    let filteredPokemon = POKEMON.filter(pokemon => pokemon.name.includes(searchBar.value))
+    let formattedPokemon = filteredPokemon.map(pokemon => makePokemonCard(pokemon))
+    pokemonContainer.innerHTML = `${formattedPokemon.join("")}`
+  }
+
+  searchBar.addEventListener("input", (e => loadPokemonToDOM()))
+
+  loadPokemonToDOM()
 })
